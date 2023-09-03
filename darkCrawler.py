3a37f179ea6c88de68acaa3a6b6b73f899a6e3cc
Warshit Poonia <98507574+warshitpoonia@users.noreply.github.com>
2023-09-03 13:33:34 +0530

    Update darkCrawler.py

--- darkCrawler.py
+++ darkCrawler.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-import requests
-from lxml import html
-from urllib.parse import urljoin, urlparse
-import collections
 import sys
-import re
 import sqlite3
+from stem import process
+from stem import Signal
+from stem.control import Controller
+from requests_socks import Session
+from bs4 import BeautifulSoup
+import scrapy
+
+# Function to renew Tor identity (get a new IP)
+def renew_tor_identity():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate()
+        controller.signal(Signal.NEWNYM)
 
 # Disable SSL warnings
 try:
@@ -18,8 +25,8 @@ except ImportError:
 START = sys.argv[1]
 
 # Set up a deque for URL queue
-urlq = collections.deque()
-urlq.append(START)
+urlq = scrapy.Queue()
+urlq.put(START)
 
 # Set to keep track of visited URLs
 visited = set()
@@ -38,38 +45,58 @@ cursor.execute('''
 ''')
 conn.commit()
 
-while urlq:
-    url = urlq.popleft()
+# Start the Tor process
+with process.launch_tor_with_config(
+        config = {
+            'SocksPort': '9050',  # Configure Tor to listen on port 9050 for SOCKS proxy
+            'ControlPort': '9051',  # Configure Tor to listen on port 9051 for control connection
+            'DataDirectory': '/tmp/tor_data'  # Optional: Specify the data directory
+        },
+        init_msg_handler = print,
+) as tor_process:
+    # Renew Tor identity to get a new IP address
+    renew_tor_identity()
 
-    try:
-        response = requests.get(url, verify=False)  # Disable SSL verification for .onion domains
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching URL {url}: {e}")
-        continue
+    class MySpider(scrapy.Spider):
+        name = 'my_spider'
+        start_urls = [START]
 
-    # Print the page title
-    try:
-        body = html.fromstring(response.content)
-        page_title = body.xpath('//title/text()')
-        print(f"Title: {page_title}")
+        def parse(self, response):
+            url = response.url
+            visited.add(url)
 
-        # Store the visited URL and page title in the database
-        cursor.execute('INSERT OR IGNORE INTO pages (url, title) VALUES (?, ?)', (url, page_title))
-        conn.commit()
+            try:
+                # Parse HTML content with BeautifulSoup4
+                soup = BeautifulSoup(response.body, 'html.parser')
+                page_title = soup.find('title').get_text()
+                print(f"Title: {page_title}")
 
-        # Extract links from the page and add them to the queue
-        links = body.xpath('//a/@href')
-        for link in links:
-            absolute_link = urljoin(url, link)
-            if absolute_link not in visited:
-                urlq.append(absolute_link)
-                visited.add(absolute_link)
+                # Store the visited URL and page title in the database
+                cursor.execute('INSERT OR IGNORE INTO pages (url, title) VALUES (?, ?)', (url, page_title))
+                conn.commit()
 
-    except Exception as e:
-        print(f"Error parsing page: {e}")
+                # Extract links from the page and add them to the queue
+                links = [urljoin(url, href) for href in soup.find_all('a', href=True)]
+                for link in links:
+                    if link not in visited:
+                        urlq.put(link)
+                        visited.add(link)
 
-# Close the database connection
-conn.close()
+            except Exception as e:
+                print(f"Error parsing page: {e}")
+
+    # Crawl using Scrapy
+    process = scrapy.crawler.CrawlerProcess({
+        'USER_AGENT': 'Mozilla/5.0',
+        'LOG_ENABLED': False,
+    })
+    process.crawl(MySpider)
+    process.start()
+
+    # Close the database connection
+    conn.close()
+
+    # Stop the Tor process
+    tor_process.terminate()
 
 print("Crawling completed.")
